1d94f910964e77b8117ad6284d82a45cfb141f1b
yongin <kkc926@naver.com>
2020-12-09 10:49:04 +0900

    backup

--- MyApplication/app/src/main/java/com/example/myapplication/navigation/AlarmFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/navigation/AlarmFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.model.AlarmDTO
 
 class AlarmFragment :Fragment(){
 
@@ -17,6 +19,31 @@ class AlarmFragment :Fragment(){
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm,container,false)
         return view
+    }
+    inner class AlarmRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var alarmDTOList : ArrayList<AlarmDTO> = arrayListOf()
+
+
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(p0.context).inflate(R.layout.item_comment,p0,false)
+            return CustomViewHolder(view)
+
+        }
+        inner class CustomViewHolder(view:View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return alarmDTOList.size
+        }
+
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            var view = p0.itemView
+//
+//            when(alarmDTOList[p1].kind){
+//                0 ->
+
+        }
+
     }
 
 }
